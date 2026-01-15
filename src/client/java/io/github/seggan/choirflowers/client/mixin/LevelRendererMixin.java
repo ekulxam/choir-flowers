15c626a2d0a91a0317499e884cf6ac65c204b827
Seggan <seggan21@gmail.com>
2026-01-15 12:21:34 -0500

    Fix placing and breaking

--- src/client/java/io/github/seggan/choirflowers/client/mixin/LevelRendererMixin.java
+++ src/client/java/io/github/seggan/choirflowers/client/mixin/LevelRendererMixin.java
@@ -1,8 +1,9 @@
 package io.github.seggan.choirflowers.client.mixin;
 
 import io.github.seggan.choirflowers.client.ChoirFlowersManager;
-import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,11 +11,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientLevel.class)
-public abstract class ClientLevelMixin {
+@Mixin(LevelRenderer.class)
+public abstract class LevelRendererMixin {
 
-    @Inject(method = "sendBlockUpdated", at = @At("HEAD"))
-    private void choirflowers$onBlockChanged(BlockPos pos, BlockState oldState, BlockState newState, int flags, CallbackInfo ci) {
+    @Inject(method = "blockChanged", at = @At("HEAD"))
+    private void choirflowers$onBlockChanged(BlockGetter level, BlockPos pos, BlockState oldState, BlockState newState, int flags, CallbackInfo ci) {
+        if (oldState == null) return;
         if (newState.is(Blocks.CHORUS_FLOWER)) {
             ChoirFlowersManager.startSinging(pos);
         } else if (oldState.is(Blocks.CHORUS_FLOWER)) {
