fc348f5cbd5da1a3bd54f9a02b600e0f49b00852
Seggan <seggan21@gmail.com>
2026-01-16 15:04:47 -0500

    Preinit

--- src/client/kotlin/io/github/seggan/choirflowers/client/ChoirFlowersClient.kt
+++ src/client/kotlin/io/github/seggan/choirflowers/client/ChoirFlowersClient.kt
@@ -2,6 +2,7 @@ package io.github.seggan.choirflowers.client
 
 import io.github.seggan.choirflowers.client.debug.DebugEntryCurrentNote
 import io.github.seggan.choirflowers.client.note.Note
+import io.github.seggan.choirflowers.client.note.Pitch
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientChunkEvents
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
@@ -13,7 +14,11 @@ import javax.sound.sampled.AudioFormat
 class ChoirFlowersClient : ClientModInitializer {
 
     override fun onInitializeClient() {
-        Note.A // Preload notes
+        for (note in Note.entries) {
+            for (octave in 2..5) {
+                Pitch(note, octave).audio
+            }
+        }
         ClientChunkEvents.CHUNK_LOAD.register { _, chunk ->
             chunk.findBlocks({ it.`is`(Blocks.CHORUS_FLOWER) }) { pos, _ ->
                 SingingChorusFlower.startSinging(pos)
